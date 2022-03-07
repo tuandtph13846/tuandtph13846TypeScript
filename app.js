6a4c681a185c6b335dca93406e23247ee98ef617
tuandtph13846 <tuandtph13846@fpt.edu.vn>
2022-03-07 14:59:53 +0700

    update app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(5, 6);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,6 @@
+function sum(a: number,b: number){
+    return a+b;
+
+}
+const result = sum(5,6);
+console.log(result);
