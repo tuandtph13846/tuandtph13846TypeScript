26c1f9d3f7ad00c13b19f1e9ea5e6624d367d6e3
tuandtph13846 <tuandtph13846@fpt.edu.vn>
2022-03-08 17:13:27 +0700

    1

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,38 @@
 "use strict";
-function sum(a, b) {
-    return a + b;
+// function sum(a: number,b: number){
+//     return a+b;
+// }
+// const a: number = 5;
+// const b: number = 6;
+// const result = sum(a,b);
+// console.log(result);
+let number1 = 5;
+let number2 = 2.8;
+let phrase = "Result is";
+let permit = true;
+const result = number1 + number2;
+if (permit) {
+    console.log(phrase + result);
 }
-const a = 5;
-const b = 6;
-const result = sum(a, b);
-console.log(result);
+else {
+    console.log('Not show result');
+}
+const person = {
+    id: 1,
+    name: "Tuan",
+    age: 20
+};
+const persons = {};
+{
+    id: 1, name;
+    "Tuan", age;
+    20, status;
+    true;
+}
+{
+    id: 2, name;
+    "Giang", age;
+    20, status;
+    false;
+}
+console.log(person, name);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,39 @@
-function sum(a: number,b: number){
-    return a+b;
+// function sum(a: number,b: number){
+//     return a+b;
 
+// }
+// const a: number = 5;
+// const b: number = 6;
+// const result = sum(a,b);
+// console.log(result);
+
+let number1 : number = 5;
+let number2 : number = 2.8;
+let phrase : string = "Result is";
+let permit : boolean = true;
+
+const result = number1 + number2;
+if(permit){
+    console.log(phrase + result);
+    
+} else {
+    console.log('Not show result');
+    
 }
-const a: number = 5;
-const b: number = 6;
-const result = sum(a,b);
-console.log(result);
+type TPerson = {
+    id: number;
+    name: string;
+    age: number;
+    status?: boolean
+}
+const person: TPerson = {
+    id: 1,
+    name:"Tuan",
+    age: 20
+}
+const persons: TPerson[] = {
+    { id: 1, name:"Tuan", age: 20,status: true},
+    { id: 2, name:"Giang", age: 20,status: false},
+}
+console.log(person.name);
+
