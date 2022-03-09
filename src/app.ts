80f07148bb3bc0259a97d3647c504d4428c4e6c1
tuandtph13846 <tuandtph13846@fpt.edu.vn>
2022-03-09 14:17:29 +0700

    update code true

--- src/app.ts
+++ src/app.ts
@@ -7,19 +7,19 @@
 // const result = sum(a,b);
 // console.log(result);
 
-let number1 : number = 5;
-let number2 : number = 2.8;
-let phrase : string = "Result is";
-let permit : boolean = true;
+// let number1 : number = 5;
+// let number2 : number = 2.8;
+// let phrase : string = "Result is";
+// let permit : boolean = true;
 
-const result = number1 + number2;
-if(permit){
-    console.log(phrase + result);
+// const result = number1 + number2;
+// if(permit){
+//     console.log(phrase + result);
     
-} else {
-    console.log('Not show result');
+// } else {
+//     console.log('Not show result');
     
-}
+// }
 type TPerson = {
     id: number;
     name: string;
@@ -31,9 +31,11 @@ const person: TPerson = {
     name:"Tuan",
     age: 20
 }
-const persons: TPerson[] = {
+const persons: TPerson[] = [
     { id: 1, name:"Tuan", age: 20,status: true},
     { id: 2, name:"Giang", age: 20,status: false},
-}
+]
+    
+
 console.log(person.name);
 
